595582678aae6c4b8f186a216351953a29d369ce
Rastisslav <bolibruch2000@gmail.com>
2022-06-06 09:37:07 +0200

    annotations first task

--- src/com/luv2code/springdemo/HockeyCoach.java
+++ src/com/luv2code/springdemo/HockeyCoach.java
@@ -1,11 +1,25 @@
 package com.luv2code.springdemo;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class HockeyCoach implements Coach{
+
+    @Autowired
+    public HockeyCoach(FortuneService theFortuneService){
+        fortuneService = theFortuneService;
+    }
+
+    private FortuneService fortuneService;
+
     @Override
     public String getDailyWorkout() {
         return "ice skate for half of hour";
+    }
+
+    @Override
+    public String getDailyFortune() {
+        return fortuneService.getFortune();
     }
 }
